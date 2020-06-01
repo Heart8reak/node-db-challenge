37d61934ec547e5be98cb7c2ff74dba73ad32265
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-06-01 01:12:32 -0400

    created project router and model

---
+++ routers/projects/projects-model.js
@@ -0,0 +1,57 @@
+const db = require('../../data/db-config')
+
+function find() {
+    return db('projects')
+}
+
+function findById(iid) {
+    return db('projects').where({ id }).first()
+}
+
+function addProject(projectData) {
+    return db('projects').insert(projectData)
+}
+
+function findProjectResources(id) {
+    return db('project-resources as pr')
+        .join('resources as r', 'r.id', 'pr.resources_id')
+        .select('r.id', 'r.resource_name', 'r.resource_description')
+        .where({ 'pr.project_id': id })
+}
+
+function findProjectTasks(id) {
+    return db('tasks as t')
+        .join('projects as p', 'p.id', 't.project_id')
+        .select('t.id', 't.task_name', 't.completed')
+        .where('p.id': id)
+}
+
+function addTasks(task, id) {
+    return db('tasks as t')
+        .join('projects as p', 'p.id', 't.project_id')
+        .select('t.id', 't.project_id', 't.task_name')
+        .where({ project_id: id })
+        .inset(task)
+}
+
+function update(change, id) {
+    return db('projects')
+        .where({ id })
+        .update(change)
+}
+
+function remove(id) {
+    return db('projects')
+        .where({ id }).del()
+}
+
+module.exports = {
+    find,
+    findById,
+    addProject,
+    findProjectResources,
+    addTasks,
+    findProjectTasks,
+    update,
+    remove
+}
---
+++ routers/projects/projects.router.js
@@ -0,0 +1,130 @@
+const express = require('express')
+
+const projects = require('./projects-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    projects.find()
+        .then((projectList) => {
+            res.json(projectList)
+        })
+        .catch((err) => {
+            res.status(500).json({ message: 'Failed to get projectList', error: err })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    projects.findById(id)
+        .then((project) => {
+            if (!project === []) {
+                res.status(404).json({ message: 'Could not find project with given ID' })
+            } else {
+                res.status(200).json(project)
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({ message: 'Failed to retreive ID from database' })
+        })
+})
+
+router.post('/', (req, res) => {
+    const projectData = req.body
+
+    projects.addProject(projectData)
+        .then((project) => {
+            res.status(201).json(({ message: 'Created new project!' }), project)
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Failed to create new project' })
+        })
+})
+
+router.get('/:id/resources', (req, res) => {
+    const { id } = req.params
+    projects.findProjectResources(id)
+        .then((resource) => {
+            if (!resource === []) {
+                res.status(404).json({ message: 'Could not retreive resource with give ID' })
+            } else {
+                res.status(200).json(resource)
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({ message: 'Problem with the ID in the database', error: err })
+        })
+})
+
+router.get('/:id/tasks', (req, res) => {
+    const { id } = req.params
+    projects.findProjectTasks(id)
+        .then((tasks) => {
+            if (!tasks === []) {
+                res.status(404).json({ message: 'Could not find task with given ID' })
+            } else {
+                res.status(200).json(tasks)
+            }
+        })
+        .catch((err) => {
+            res.status(500).sjon({ message: 'Problem with the ID in the database' })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+
+    projects.findById(id)
+        .then((project) => {
+            if (project) {
+                projects.update(changes, id)
+                    .then((updatedproject) => {
+                        res.json(updatedproject)
+                    })
+            } else {
+                res.status(404).json({ message: 'Could not find project with given ID' })
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Failed to update project' })
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+
+    projects.remove(id)
+        .then((deleted) => {
+            if (deleted) {
+                res.json({ remove: deleted })
+            } else {
+                res.status(404).json({ message: 'Could not find project with given ID' })
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Failed to deleted project' })
+        })
+})
+
+router.post('/:id/tasks', (req, res) => {
+    const taskData = req.body
+    const { id } = req.params
+
+    projects.findById(id)
+        .then((project) => {
+            if (project) {
+                projects.addTasks(taskData, id)
+                    .then((step) => {
+                        res.status(201).json(step)
+                    })
+            } else {
+                res.status(404).json({ message: 'Could not find project with given ID' })
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Failed to create new step' })
+        })
+})
+
+module.exports = router
